502067caa8fa2625820a96339f0fc3ad2f1ef8f4
Chris Mague <chris@mague.com>
2011-09-11 17:39:13 -0700

    almost there

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f ${HOME}/.dotfiles ] ; then
+	echo "NO DOTFILES - checking out for you"
+	cd ${HOME} && git checkout git@github.com:maguec/dotfiles.git .dotfiles
+fi
+
+#setup vim
+ln -s ${HOME}/.dotfiles/vim/vimrc ${HOME}/.vimrc
+ln -s ${HOME}/.dotfiles/vim/gvimrc ${HOME}/.gvimrc
+ln -s ${HOME}/.dotfiles/vim ${HOME}/.vim
+
+#setup bash
+ln -s ${HOME}/.dotfiles/bash/bashrc ${HOME}/.bashrc
+ln -s ${HOME}/.dotfiles/bash/profile ${HOME}/.bash_profile
+
+#setup mutt
+ln -s ${HOME}/.dotfiles/mutt/mutt ${HOME}/.muttrc
+
